a62aea715e1b6e4b7a828ef37d38b9d22d70a524
Hamza Al-Sarsour <hamze.85@live.com>
2024-05-15 05:17:48 +0300

    Completing Repositories ILeaveRequestRepository...

--- ASP.Net Core 7 SOLID (Onion Arc)/HRLeaveManagementCLean/HRLeaveManagementCLean/src/Infrastructure/HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
+++ ASP.Net Core 7 SOLID (Onion Arc)/HRLeaveManagementCLean/HRLeaveManagementCLean/src/Infrastructure/HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -12,5 +12,21 @@ public class LeaveRequestRepository : GenericRepository<LeaveRequest>, ILeaveReq
 
     }
 
+    public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails()
+    {
+        var leaveRequests = await _context.LeaveRequests.Include(q => q.LeaveType).ToListAsync();
+        return leaveRequests;
+    }
 
+    public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails(string userId)
+    {
+        var leaveRequests = await _context.LeaveRequests.Where(q => q.RequestingEmployeeId == userId).Include(q => q.LeaveType).ToListAsync();
+        return leaveRequests;
+    }
+
+    public async Task<LeaveRequest> GetLeaveRequestWithDetails(int id)
+    {
+        var leaveRequest = await _context.LeaveRequests.Include(q => q.LeaveType).FirstOrDefaultAsync(q => q.Id == id);
+        return leaveRequest;
+    }
 }
